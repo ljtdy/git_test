48077806644b89ecceec24eb7cf2a94b5c4f2efd
wade <1696112459@qq.com>
2023-02-19 23:19:34 +0800

    hot-fix git

--- src/main/java/com/ljt/git/HelloGit.java
+++ src/main/java/com/ljt/git/HelloGit.java
@@ -9,5 +9,6 @@ public class HelloGit {
         System.out.println("hello,我是git");
         System.out.println("我是second");
         System.out.println("我是tried");
+        System.out.println("我是分支hot-fix测试");
     }
 }
