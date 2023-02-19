118292db93e7083b68faa84a7f2bfce46e7bfaf4
wade <1696112459@qq.com>
2023-02-19 23:15:10 +0800

    first git

---
+++ src/main/java/com/ljt/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.ljt.git;
+
+
+
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello,我是git");
+        System.out.println("hello,我是git");
+    }
+}
