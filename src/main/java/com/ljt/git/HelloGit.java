e4fe41e66d46f5ad8b2842b8ccabe5c8fec5df95
wade <1696112459@qq.com>
2023-02-19 23:16:10 +0800

    second git

--- src/main/java/com/ljt/git/HelloGit.java
+++ src/main/java/com/ljt/git/HelloGit.java
@@ -7,5 +7,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello,我是git");
         System.out.println("hello,我是git");
+        System.out.println("我是second");
     }
 }
