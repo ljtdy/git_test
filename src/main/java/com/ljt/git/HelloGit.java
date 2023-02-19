26327e847e5c3e693264e30860a749cb6ae579b9
wade <1696112459@qq.com>
2023-02-19 23:17:06 +0800

    trice git

--- src/main/java/com/ljt/git/HelloGit.java
+++ src/main/java/com/ljt/git/HelloGit.java
@@ -8,5 +8,6 @@ public class HelloGit {
         System.out.println("hello,我是git");
         System.out.println("hello,我是git");
         System.out.println("我是second");
+        System.out.println("我是tried");
     }
 }
